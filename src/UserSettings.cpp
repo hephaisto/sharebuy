1d998668eac3fd0b293327a1729036267d2b54d6
hephaisto <hephaisto@users.noreply.github.com>
2016-05-06 20:45:42 +0200

    fix: show current about text in settings

--- src/UserSettings.cpp
+++ src/UserSettings.cpp
@@ -15,20 +15,26 @@ UserSettings::UserSettings(Wt::WContainerWidget *parent)
 	new Wt::WText("<h1>settings</h1>", this);
 	
 	Session& dbSession = static_cast<ShareBuy*>(Wt::WApplication::instance())->dbSession;
+	dbo::Transaction transaction(dbSession);
 	const Wt::Auth::User &user = dbSession.login().user();
+
+	PUser u = dbSession.user();
 
 	Wt::WPanel *passwordPanel = new Wt::WPanel(this);
 	passwordPanel->setTitle("Change Password");
 	Wt::WWidget* changePassword=static_cast<ShareBuy*>(Wt::WApplication::instance())->getAuthWidget()->createUpdatePasswordView(user, false);
 	passwordPanel->setCentralWidget(changePassword);
 
+
 	Wt::WPanel *aboutPanel = new Wt::WPanel(this);
 	aboutPanel->setTitle("More info about yourself (about)");
 	Wt::WTextArea *ta = new Wt::WTextArea();
+
+	ta->setText(u->about);
 	ta->changed().connect(std::bind([&dbSession, ta]()
 	{
 		dbo::Transaction transaction(dbSession);
-		dbSession.user().modify()-> about = ta->text().toUTF8();
+		dbSession.user().modify()->about = ta->text().toUTF8();
 	}));
 	aboutPanel->setCentralWidget(ta);
 }
