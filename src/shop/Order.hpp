49d4af28b5c3d37dac4bb72f8a4ee8a7396f2762
hephaisto <hephaisto@users.noreply.github.com>
2016-06-16 05:26:55 +0200

    Add time properties to order class

--- src/shop/Order.hpp
+++ src/shop/Order.hpp
@@ -22,10 +22,14 @@ public:
 
 	dbo::ptr<User> user;
 	dbo::collection<dbo::ptr<Item> > items;
+	Wt::WDateTime orderTime;
+	Wt::WDateTime receiveTime;
 
 	template<class Action>
 	void persist(Action& a)
 	{
+		dbo::field(a, orderTime, "orderTime");
+		dbo::field(a, receiveTime, "receiveTime");
 		dbo::belongsTo(a, user, "user");
 		dbo::hasMany(a, items, dbo::ManyToOne, "ordering");
 	}
