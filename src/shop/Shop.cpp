hephaisto/sharebuy

commit 8266ca71baba50301f95bb675b72218ec338369d

tool to check regexes in the files

src/SConscript
@@ -51,6 +51,31 @@ libs=[
 sharebuy=env.Program(target="sharebuy",source=sources, LIBS=libs)
 Default(sharebuy)
 
+# check regex
+check_regex_sources = [
+"tools/check_regex.cpp",
+"shop/Shop.cpp",
+#"shop/BasketAddTemplate.cpp",
+#"shop/BasketFormTemplate.cpp",
+#"shop/Item.cpp",
+]
+
+check_regex_libs = [
+"wt",
+"wtdbo",
+"boost_system",
+"boost_filesystem",
+"boost_thread",
+"boost_regex",
+"boost_signals",
+"boost_log",
+"boost_log_setup",
+"pthread",
+]
+env.Append(CCFLAGS="-DDO_NOT_COMPILE_APP")
+check_regex = env.Program(target="check_regex", source=check_regex_sources, LIBS=check_regex_libs)
+env.Alias("check_regex", check_regex)
+
 # package
 shop_configs = glob.glob("../shops/*")
 print("x",shop_configs)

src/shop/Shop.cpp
@@ -10,7 +10,9 @@ namespace bf=boost::filesystem;
 
 Shop::Shop(bpt::ptree &pt)
 :name(pt.get<string>("name")),
+#if !defined DO_NOT_COMPILE_APP
 basketAdd(std::make_shared<BasketFormTemplate>(pt.get_child("basket_form"))),
+#endif
 inputURLRegex{pt.get<string>("input_url_regex")},
 pageRegex{pt.get<string>("page_regex")},
 imageTemplate{pt.get<string>("item_image_template")}

src/shop/Shop.hpp
@@ -17,7 +17,9 @@ class Shop
 public:
 	explicit Shop(bpt::ptree &pt);
 	const string name;
+#if !defined DO_NOT_COMPILE_APP
 	const shared_ptr<const BasketAddTemplate> basketAdd;
+#endif
 	const boost::regex inputURLRegex;
 	boost::format imageTemplate;
 	const boost::regex pageRegex;

src/tools/check_regex.cpp
@@ -1,0 +1,150 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <boost/foreach.hpp>
+#include <Wt/Utils>
+#include <Wt/WIntValidator>
+#include <Wt/WPushButton>
+#include <Wt/Http/Client>
+#include <Wt/Http/Message>
+#include <Wt/WIOService>
+#include <boost/log/trivial.hpp>
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/filesystem.hpp>
+namespace bf=boost::filesystem;
+#include <fstream>
+#include <limits>
+
+#include "../shop/Shop.hpp"
+
+using std::string;
+
+int main(int argc, char **argv)
+{
+	if(argc < 3)
+	{
+		std::cerr<< "USAGE: "<< argv[0] << "shopfile url\n";
+		return 1;
+	}
+
+	bpt::ptree pt;
+	bpt::read_info(bf::path(argv[1]).string(),pt);
+	shared_ptr<Shop> shop = std::make_shared<Shop>(pt);
+
+	string newText = Wt::Utils::urlDecode(argv[2]);
+	std::cout<<"changed to "<<newText<<"\n";
+	boost::smatch url_match;
+	if(!boost::regex_search(newText,url_match, shop->inputURLRegex))
+	{
+		std::cerr << "url does not match supplied regex\n";
+		return 3;
+	}
+
+	Wt::WIOService service;
+	service.start();
+	service.initializeThread();
+
+	std::cout<<"match for shop "<<shop->name<<"\n";
+	std::cout<<"number: "<<url_match["number"]<<"\n";
+	std::cout<<"number2: "<<url_match["number2"]<<"\n";
+
+	std::map<string, string> urlMatches;
+	if(url_match["number"].matched)
+		urlMatches["number"]=url_match["number"];
+	if(url_match["number2"].matched)
+		urlMatches["number2"]=url_match["number2"];
+	if(url_match["title"].matched)
+		urlMatches["title"]=url_match["title"];
+	
+	// get data from page
+	Wt::Http::Client *client = new Wt::Http::Client(service);
+	client->setTimeout(10);
+	client->setMaximumResponseSize(1024*1024);
+	client->done().connect([ client, newText, url_match, shop, urlMatches ](boost::system::error_code err, const Wt::Http::Message& response, Wt::NoClass, Wt::NoClass, Wt::NoClass, Wt::NoClass)
+	{
+		std::cout<<"done()\n";
+
+		if(!err)
+		{
+			if(response.status() == 200)
+			{
+				std::cout<<"shop regexes:\n";
+				boost::smatch pageMatch;
+				if(boost::regex_search(response.body(), pageMatch, shop->pageRegex))
+				{
+					std::map<string, string> matches = urlMatches;
+
+					// number
+					if(pageMatch["number"].matched)
+					{
+						matches["number"]=pageMatch["number"];
+					}
+
+					// number2
+					if(pageMatch["number2"].matched)
+					{
+						matches["number2"]=pageMatch["number2"];
+					}
+
+					// price
+					if(pageMatch["price"].matched)
+					{
+						matches["price"]=pageMatch["price"];
+					}
+
+					// title
+					if(pageMatch["title"].matched)
+					{
+						matches["title"]=pageMatch["title"];
+					}
+
+					BOOST_FOREACH(auto m, matches)
+						std::cout<<"matches[\""<<m.first<<"\"]=\""<<m.second<<"\"\n";
+
+					double price=std::numeric_limits<double>::quiet_NaN();
+					try
+					{
+						price = boost::lexical_cast<double>(matches["price"]);
+					}
+					catch(boost::bad_lexical_cast &)
+					{
+						std::cerr<<"unable to convert \""<<matches["price"]<<"\" to float\n";
+						try // try to fix "," as a comma separator
+						{
+							matches["price"].replace(matches["price"].find(','),1,{'.'});
+							std::cerr<<"trying with \""<<matches["price"]<<"\"\n";
+							price = boost::lexical_cast<double>(matches["price"]);
+						}
+						catch(boost::bad_lexical_cast &)
+						{}
+					}
+
+					std::cout<<"Success!\n";
+					// TODO print parsed results
+
+				}
+				else
+				{
+					BOOST_LOG_TRIVIAL(fatal)<<"unable to match page regex to shop response for shop "<<shop->name;
+					std::ofstream responseFile("unmatched_response");
+					responseFile<<response.body();
+				}
+			}
+			else
+				std::cout<<"response statuscode: "<<response.status()<<"\n";
+		}
+		else
+			std::cout<<"error: "<<err.message()<<"\n";
+		delete client;
+	});
+	if(!client->get(argv[2]))
+	{
+		std::cerr<<"unable to start client\n";
+		return 4;
+	}
+	//std::cout<<"waiting...\n";
+	//std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+	service.stop();
+	std::cout<<"end\n";
+}
+

commit 5a988ffb2eccba94fd135431abe7b0d2cccc061b

fix build error due to #ifndef error
introduced by 8266ca71baba50301f95bb675b72218ec338369d

src/SConscript
@@ -11,7 +11,7 @@ env.Append(CCFLAGS="-DBOOST_ALL_DYN_LINK")
 
 env.Substfile(target="version.hpp",source="version.hpp.in")
 
-sources=[
+sources_main=[
 "shop/BasketListWidget.cpp",
 "shop/BasketAddTemplate.cpp",
 "shop/BasketFormTemplate.cpp",
@@ -26,11 +26,9 @@ sources=[
 "user/Session.cpp",
 
 "application.cpp",
-"sharebuy.cpp",
 "UserSettings.cpp"
 ]
 
-print(sources)
 libs=[
 "wt",
 "wthttp",
@@ -48,32 +46,11 @@ libs=[
 "wtdbosqlite3"
 ]
 
-sharebuy=env.Program(target="sharebuy",source=sources, LIBS=libs)
+sharebuy=env.Program(target="sharebuy",source=sources_main+["sharebuy.cpp"], LIBS=libs)
 Default(sharebuy)
 
 # check regex
-check_regex_sources = [
-"tools/check_regex.cpp",
-"shop/Shop.cpp",
-#"shop/BasketAddTemplate.cpp",
-#"shop/BasketFormTemplate.cpp",
-#"shop/Item.cpp",
-]
-
-check_regex_libs = [
-"wt",
-"wtdbo",
-"boost_system",
-"boost_filesystem",
-"boost_thread",
-"boost_regex",
-"boost_signals",
-"boost_log",
-"boost_log_setup",
-"pthread",
-]
-env.Append(CCFLAGS="-DDO_NOT_COMPILE_APP")
-check_regex = env.Program(target="check_regex", source=check_regex_sources, LIBS=check_regex_libs)
+check_regex = env.Program(target="check_regex", source=sources_main+["tools/check_regex.cpp"], LIBS=libs)
 env.Alias("check_regex", check_regex)
 
 # package

src/shop/Shop.cpp
@@ -10,9 +10,7 @@ namespace bf=boost::filesystem;
 
 Shop::Shop(bpt::ptree &pt)
 :name(pt.get<string>("name")),
-#if !defined DO_NOT_COMPILE_APP
 basketAdd(std::make_shared<BasketFormTemplate>(pt.get_child("basket_form"))),
-#endif
 inputURLRegex{pt.get<string>("input_url_regex")},
 pageRegex{pt.get<string>("page_regex")},
 imageTemplate{pt.get<string>("item_image_template")}

src/shop/Shop.hpp
@@ -17,9 +17,7 @@ class Shop
 public:
 	explicit Shop(bpt::ptree &pt);
 	const string name;
-#if !defined DO_NOT_COMPILE_APP
 	const shared_ptr<const BasketAddTemplate> basketAdd;
-#endif
 	const boost::regex inputURLRegex;
 	boost::format imageTemplate;
 	const boost::regex pageRegex;